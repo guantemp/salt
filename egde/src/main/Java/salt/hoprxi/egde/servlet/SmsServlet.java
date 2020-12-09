6b268991c891d9a610cf83f63d12c29cf56ac507
guantemp <guantemp@126.com>
2020-12-09 22:29:23 +0800

    202012092229

--- egde/src/main/Java/salt/hoprxi/egde/servlet/SmsServlet.java
+++ egde/src/main/Java/salt/hoprxi/egde/servlet/SmsServlet.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2020. www.hoprxi.com All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package salt.hoprxi.egde.servlet;
 
 import com.aliyuncs.CommonRequest;
@@ -29,7 +44,7 @@ import java.util.regex.Pattern;
  * @since JDK8.0
  */
 @WebServlet(urlPatterns = {"/v1/sms"}, name = "sms", asyncSupported = false, initParams = {
-        @WebInitParam(name = "expired", value = "5*60*1000"), @WebInitParam(name = "accessKey", value = ""),
+        @WebInitParam(name = "expire", value = "5*60*1000"), @WebInitParam(name = "accessKey", value = ""),
         @WebInitParam(name = "secret", value = ""), @WebInitParam(name = "signName", value = "ABC商城"),
         @WebInitParam(name = "templateCode", value = "SMS_206562265")})
 public class SmsServlet extends HttpServlet {
--- egde/src/main/Java/salt/hoprxi/egde/servlet/WxAuthServlet.java
+++ egde/src/main/Java/salt/hoprxi/egde/servlet/WxAuthServlet.java
@@ -1,6 +1,24 @@
+/*
+ * Copyright (c) 2020. www.hoprxi.com All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package salt.hoprxi.egde.servlet;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,10 +29,27 @@ import java.io.IOException;
  * @version 0.0.1 2020-12-09
  * @since JDK8.0
  */
+@WebServlet(urlPatterns = {"/v1/wxAuth"}, name = "wxAuth", asyncSupported = false, initParams = {
+        @WebInitParam(name = "appid", value = ""), @WebInitParam(name = "secret", value = "")})
 public class WxAuthServlet extends HttpServlet {
+    private static String appid = null;
+    private static String secret = null;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        if (config != null) {
+            appid = config.getInitParameter("appid");
+            secret = config.getInitParameter("secret");
+        }
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        String code = req.getParameter("code");
+        if (code == null || code.isEmpty()) {
+
+        }
     }
 
     @Override
