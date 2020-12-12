178a8fb120c3011782e6caf248b297882377b479
guantemp <guantemp@126.com>
2020-12-12 22:46:05 +0800

    202012122245

--- egde/src/main/Java/salt/hoprxi/egde/servlet/WxAuthServlet.java
+++ egde/src/main/Java/salt/hoprxi/egde/servlet/WxAuthServlet.java
@@ -15,6 +15,18 @@
  */
 package salt.hoprxi.egde.servlet;
 
+import com.fasterxml.jackson.core.JsonEncoding;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
@@ -23,6 +35,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * @author <a href="www.hoprxi.com/author/guan xianghuang">guan xiangHuan</a>
@@ -30,7 +44,7 @@ import java.io.IOException;
  * @since JDK8.0
  */
 @WebServlet(urlPatterns = {"/v1/wxAuth"}, name = "wxAuth", asyncSupported = false, initParams = {
-        @WebInitParam(name = "appid", value = ""), @WebInitParam(name = "secret", value = "")})
+        @WebInitParam(name = "appid", value = "wx16772085b996e8e0"), @WebInitParam(name = "secret", value = "")})
 public class WxAuthServlet extends HttpServlet {
     private static String appid = null;
     private static String secret = null;
@@ -47,9 +61,39 @@ public class WxAuthServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String code = req.getParameter("code");
+        JsonFactory jasonFactory = new JsonFactory();
+        resp.setContentType("application/json; charset=UTF-8");
+        JsonGenerator generator = jasonFactory.createGenerator(resp.getOutputStream(), JsonEncoding.UTF8)
+                .setPrettyPrinter(new DefaultPrettyPrinter());
         if (code == null || code.isEmpty()) {
-
+            generator.writeNumberField("code", 401);
+            generator.writeStringField("msg", "网络错误！验证码未能发送，请稍后再试。");
+        } else {
+            try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+                URI uri = new URIBuilder()
+                        .setScheme("https")
+                        .setHost("api.weixin.qq.com")
+                        .setPathSegments("sns", "jscode2session")
+                        .setParameter("grant_type", "authorization_code")
+                        .setParameter("appid", appid)
+                        .setParameter("secret", secret)
+                        .setParameter("code", code)
+                        .build();
+                //System.out.println(uri.toURL().toExternalForm());
+                HttpGet httpGet = new HttpGet(uri);
+                try (CloseableHttpResponse response = httpclient.execute(httpGet)) {
+                    System.out.println(response.getEntity());
+                    HttpEntity entity = response.getEntity();
+                    // do something useful with the response body
+                    // and ensure it is fully consumed
+                    EntityUtils.consume(entity);
+                }
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
         }
+        generator.flush();
+        generator.close();
     }
 
     @Override
--- egde/src/main/webapp/index.jsp
+++ egde/src/main/webapp/index.jsp
@@ -4,6 +4,9 @@
 </head>
 <body>
 <h2>Hello World!</h2>
-<a href="v1/sms?mobile=13679692301">validate</a>
+<h3>
+    <a href="v1/sms?mobile=13679692301">validate</a></h3>
+<p>
+    <a href="v1/wxAuth?code=13679692301">Wx_AUTH</a></p>
 </body>
 </html>
