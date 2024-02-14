3437cd00f52c9e373fd8783bbf92b704d11377f7
guan <guantemp@126.com>
2024-02-14 21:16:53 +0800

    202402142116

--- id/src/test/java/salt/hoprxi/id/LongIdTest.java
+++ id/src/test/java/salt/hoprxi/id/LongIdTest.java
@@ -20,10 +20,12 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
 
 
 /***
@@ -57,8 +59,10 @@ public class LongIdTest {
         System.out.println("Process id：" + Process.process());
         System.out.println("Recover time from Identity:" + LongId.timestamp(LongId.generate()));
 
-        //System.out.println("2015-03-26 00:00:00(UTC/GMT+08:00) to long:"
-                //+ new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format("").to);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        simpleDateFormat.setTimeZone(TimeZone.getDefault());
+        System.out.println("2015-03-26 00:00:00(UTC/GMT) to long:"
+                + simpleDateFormat.parse("2015-03-26 00:00:00.000").getTime());
         System.out.println("2015-03-26 00:00:00(UTC/GMT+08:00) to long:"
                 + LocalDateTime.parse("2015-03-26 00:00:00.000", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")).atZone(ZoneId.of("UTC")).toInstant().toEpochMilli());
         System.out.println("1427328000000L to date: " + Instant.ofEpochMilli(1427328000000L));
